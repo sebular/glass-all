d27bcaaf1556d1c16e05e719f56fa6cde772dfcf
Sebastian <sebastian.k.wood@gmail.com>
2012-10-04 11:01:31 -0700

    commit before trying to add shatter effect to player

--- glass/src/com/sebastian/glass/Player.java
+++ glass/src/com/sebastian/glass/Player.java
@@ -6,11 +6,14 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.RayCastCallback;
 
-public class Player extends GameObject {
+public class Player extends GameObject implements RayCastCallback {
 	boolean boosting = false;
 	boolean turningLeft = false;
 	boolean turningRight = false;
+	int SHATTER_PIECES = 5;
 	
 	public Player(Glass game, Vector2 position) {
 		super(game, position, new Vector2[3]);
@@ -21,8 +24,9 @@ public class Player extends GameObject {
 		this.scale = .5f;
 		this.vertices[0] = new Vector2(-1 * scale, -1 * scale);
 		this.vertices[1] = new Vector2(1 * scale, -1 * scale);
-		this.vertices[2] = new Vector2(0 * scale, 2 * scale); 
+		this.vertices[2] = new Vector2(0 * scale, 2 * scale); 		
 		this.unrotatedVertices = this.vertices.clone();
+		
 		initRender();
 		initPhysics();
 	}
@@ -69,7 +73,24 @@ public class Player extends GameObject {
 	
 	public void initPhysics() {
 		this.addComponent(ComponentType.PhysicsComponent);
-		physicsComponent.createPolygonBody("shape", position.x, position.y, vertices, 1, BodyType.DynamicBody);
+		physicsComponent.createPolygonBody("shape", position.x, position.y, unrotatedVertices, 1, BodyType.DynamicBody);
 		physicsComponent.body.setFixedRotation(true);
+	}
+	
+	public void shatter(float x, float y) {
+		for (int i = 0; i < SHATTER_PIECES; i++) {
+			float cutAngle = MathUtils.random() * MathUtils.PI * 2;
+			Vector2 point1 = new Vector2(0,0);
+			Vector2 point2 = new Vector2(0,0);
+			game.world.rayCast(this, point1, point2);
+		}
+	}
+
+	@Override
+	public float reportRayFixture(Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
+		if (this.physicsComponent.body == fixture.getBody()) {
+			
+		}
+		return 0;
 	}
 }
